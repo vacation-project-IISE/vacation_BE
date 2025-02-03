bf09850a98c6a6cfc602973d7c23a53f3795e07b
LAICME <ehxhfks@naver.com>
2025-02-03 11:06:04 +0900

    routes 전체 업로드

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -4,5 +4,6 @@ const emailController = require("../controllers/emailController");
 
 router.post("/findId", emailController.emailAuthId);
 router.post("/findPw", emailController.emailAuthPw);
+router.post("/resetPW", emailController.resetPassword); // 비밀번호 변경 추가
 
 module.exports = router;
